9833b9b2ccef6fdfe426f4c9cf650b484b6db8d9
Nikki Koole <nikki@floorplanner.com>
2015-10-02 00:21:15 +0100

    just before browserify

--- todo_benchmark/bundle.js
+++ todo_benchmark/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var hello = require('./simple_lib').hello;
 
 
@@ -194,3 +195,10 @@ var setProps = function(element, properties) {
 var replace = function(elemOld, elemNew) {
     elemOld.parentNode.replaceChild(elemNew, elemOld);
 };
+
+},{"./simple_lib":2}],2:[function(require,module,exports){
+module.exports = {
+    hello:function(){console.log("ah yes")}
+};
+
+},{}]},{},[1])
