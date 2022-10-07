f228fb7874f5268f6704f529bb00555ffc90e41f
Nikki Koole <nikki@floorplanner.com>
2022-10-07 13:59:52 +0200

    removed everything, moved static site stuff in

--- emacs/emacsDOTandD/.emacs.d/elpa/queue-0.1.1/queue-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; queue-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "queue" "queue.el" (21649 33665 0 0))
-;;; Generated autoloads from queue.el
-
-(defalias 'make-queue 'queue-create "\
-Create an empty queue data structure.")
-
-;;;***
-
-;;;### (autoloads nil nil ("queue-pkg.el") (21649 33665 396890 0))
-
-;;;***
-
-(provide 'queue-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; queue-autoloads.el ends here
---
+++ host.lua
@@ -0,0 +1,2 @@
+os.execute("cd public &&  python -m SimpleHTTPServer")
+os.execute("cd public &&  python3 -m http.server")
