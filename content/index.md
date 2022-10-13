67e556dfe323de4895dd00fe7a292fd991ba59a7
Nikki Koole <nikki@floorplanner.com>
2022-10-13 11:08:49 +0200

    improved calendar a bit

--- content/index.md
+++ content/index.md
@@ -1,36 +1,33 @@
 
 <div class='put-calendar-in-here'></div>
 <script>
-//let deadlines = [new Date(2022, 9, 31),new Date(2022, 9, 1), new Date(2022, 9, 15)];
-let deadlines = [new Date(2022, 9, 20),new Date(2022, 9, 31)];
-
-buildCalendar(deadlines, new Date())
+	let deadlines = [{date:new Date(2022, 9, 31), description: 'this website'}];
+	buildCalendar(deadlines, new Date());
 </script>
 
 
 ## Hello, my name is Nikki 
 
-Once a talented artist, now a front end developer.  
-But I got dreams.  
-And they make me go mad.  
+Once a talented artist, now a front end developer.
+But I got dreams.
+And they make me go mad.
 
-Anyway, I am building **better** child apps,  
-There are nice ones out there, but they look too generic.  
-There are good looking ones, but they don't feel great.  
-There are even better ones, but they feel dumb somehow.  
+Anyway, I am building **better** children apps,
+There are nice ones out there, but they look too generic.
+There are good looking ones, but they don't feel great.
+There are even better ones, but they feel dumb somehow.
+And then there is the vast majority that just tries to throw in as many advertisements and micorpayments as possible.
 
-I know children, matter of fact, bit of a big one myself and I love developing, drawing, animating and making music.  
-So I feel I can do better.  
+I know children, matter of fact, bit of a big one myself and I love developing, drawing, animating and making music.
+So I feel I can do better.
 
-I've been working for quite some time on tooling (an animation tool, a vector drawing tool, a music making tool)  
+I've been working for quite some time on tooling (an animation tool, a vector drawing tool, a music making tool)
 Now I feel the procrastination phase is done, and apps need to end up in the app store before soon.
 
 
-
-
-So let's introduce some deadlines,  
-and let's make them thight.  
-See that calendar above? I'll be adding deadlines as we go.  
+So let's introduce some deadlines,
+and let's make them thight.
+See that calendar above? I'll be adding deadlines as we go.
 Pumping out apps, one per month, the first deadline is about this website, I'll be writing here about my process, and need a place where I can keep track about other stuff too. 
 
 End of the month this website will be live, and sice I am quite far already and I also need some extra work for all other apps, I'll add 'figuring out how to deploy apps and do a test run' to the first deadline too.
--- docs/calendar.js
+++ docs/calendar.js
@@ -1,17 +1,14 @@
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
-//const dayNames = ["Sun", 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const dayNames = ["zo", "ma", "di", "wo", "do", "vr", "za"]
 
 function getDaysInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
-
 function getFirstDayOfMonth(year, month) {
   return new Date(year, month, 1).getDay();
 }
-
 
 // you feed it a list of deadlines, and a current Date
 // then you can toggle and look to older/newer months
@@ -25,12 +22,25 @@ function buildCalendar(deadlines, date) {
         let closestInFuture = Number.MAX_SAFE_INTEGER;
         let now = new Date()
         deadlines.forEach(d =>{
-            if (now < d) { // only interessted in future events
-            if (d.getTime() < closestInFuture) {
-                closestInFuture = d.getTime()
+            if (now < d.date) { // only interessted in future events
+            if (d.date.getTime() < closestInFuture) {
+                closestInFuture = d.date.getTime()
             }}
         })
-        return (new Date(new Date(closestInFuture) - new Date()).getDate())
+        if (closestInFuture == Number.MAX_SAFE_INTEGER) {
+            // then we just find the most recent deadline that has passed
+            deadlines.forEach(d =>{
+                if (d.date.getTime() < closestInFuture) {
+                    closestInFuture = d.date.getTime()
+                }});
+            // in case there arent any deadlines set (past or future)
+            if (closestInFuture == Number.MAX_SAFE_INTEGER) {
+                return undefined;
+            }
+           return -1 * (new Date(new Date() - new Date(closestInFuture) ).getDate())
+        } else {
+             return (new Date(new Date(closestInFuture) - new Date()).getDate())
+        }
     }
     
     const firstDayCurrentMonth = getFirstDayOfMonth(
@@ -54,20 +64,23 @@ function buildCalendar(deadlines, date) {
     });
     let countTo = getDaysInMonth( date.getFullYear(),date.getMonth()+1)
     for (let i=1;i<=countTo;i++) {
-	let li= document.createElement('li')
+	let li= document.createElement('li');
+        li.innerHTML = i;
 	if (i==1) {
-            li.style="grid-column-start:"+(firstDayCurrentMonth+1)
+            li.style="grid-column-start:"+(firstDayCurrentMonth+1);
 	}
         deadlines.forEach(deadline => {
-            if (deadline.getMonth() == date.getMonth() && i == deadline.getDate() ) { 
-	        li.className="deadline"
+            if (deadline.date.getMonth() == date.getMonth() && i == deadline.date.getDate() ) { 
+	        li.className="deadline";
+                li.innerHTML += ` <div class='description'>${deadline.description}</div>` 
             }
         })
         if (i==new Date().getDate() && date.getMonth() == new Date().getMonth()) {
-	    li.className="today"
+	    li.className="today";
+            li.innerHTML += ` <div class='description'>today</div>` 
 	}
-	li.innerHTML = i
-	ul.appendChild(li)
+
+	ul.appendChild(li);
     }
     c.appendChild(ul);
 
@@ -77,7 +90,8 @@ function buildCalendar(deadlines, date) {
     
 
     let next = document.createElement('h2');
-    next.innerHTML = getDaysUntilNextDeadLine()+ " days until next deadline."
+    let daysUntil = getDaysUntilNextDeadLine()
+    next.innerHTML =  daysUntil == undefined ? ('no deadlines') : daysUntil >= 0 ?  (daysUntil+" days until next deadline.") : (-1*daysUntil+" days passed since deadline. 😔")
 
     let node = document.querySelector('.put-calendar-in-here')
 
